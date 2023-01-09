b9af70eca5993e96d0f8a7e673dbc0fcdc9a5279
Patcharee2643 <patcharee.nam@ku.th>
2023-01-09 12:09:26 +0700

    https://github.com/Patcharee2643/Lab2.git

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action="show.php">
+        What is your name:<input type="text" name="name"><br>
+        What is your age:<input type="text" name="age"><br>
+        What is your age:<input type="text" name="age"><br>
+        What is your age:<input type="text" name="age"><br>
+        
+        <input type="Submit" value="Submit">
+    </form>
+
+   
+</body>
+
+</html>
---
+++ show.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    $name = $_GET["name"];
+    $age = $_GET["age"];
+    $male = $_GET["male"];
+    echo "Name:".$name."<br>";
+    echo "Age:".$age."<br>" ;
+    echo "Name:".$name."<br>";
+    ?>
+</body>
+
+</html>
