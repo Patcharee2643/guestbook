2eefd48fe8afd9490954dc228e7412123722e90b
Patcharee2643 <95962908+Patcharee2643@users.noreply.github.com>
2023-01-16 14:35:21 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -5,10 +5,48 @@
     <?php
     $name = $_GET["name"];
     $age = $_GET["age"];
-    $male = $_GET["male"];
-    echo "Name:".$name."<br>";
-    echo "Age:".$age."<br>" ;
-    echo "Name:".$name."<br>";
+    $your_gender= $_GET["your_gender"];
+    $married =$_GET["married"];
+    
+
+    if ($age < 15 && $your_gender=="ชาย"&& $married=="ยังไม่แต่งงาน")
+    {
+        echo "เด็กชาย:".$name."<br>";
+
+    }
+    else if ($age >= 15 && $your_gender=="ชาย"&& $married=="ยังไม่แต่งงาน")
+    {
+        echo "นาย:".$name."<br>";
+
+    }
+    else if ($age >= 15 && $your_gender=="ชาย"&& $married=="แต่งงานแล้ว")
+    {
+        echo "นาย:".$name."<br>";
+
+    }
+    else if($age < 15 && $your_gender=="หญิง"&& $married=="ยังไม่แต่งงาน")
+    {
+        echo "เด็กหญิง:".$name."<br>";
+
+    }
+    else if($age >= 15 && $your_gender=="หญิง"&& $married=="ยังไม่แต่งงาน")
+    {
+        echo "  นางสาว:".$name."<br>";
+
+
+    }
+    else if($age >= 15 && $your_gender=="หญิง"&& $married=="แต่งงานแล้ว")
+    {
+        echo "  นาง:".$name."<br>";
+
+
+    }
+
+    
+    echo "อายุ:".$age." ปี"."<br>";
+    echo "เพศ:".$your_gender."<br>";
+    echo  $married ."<br>";
+    
     ?>
 </body>
 
