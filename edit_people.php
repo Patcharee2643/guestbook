445bcf0258cb7ea4da280775fdf5b6ce533e3ad3
Patcharee2643 <patcharee.nam@ku.th>
2023-02-13 10:58:24 +0700

    M

--- edit_people.php
+++ edit_people.php
@@ -9,7 +9,7 @@ if($result->num_rows > 0){
     $row = $result->fetch_assoc();
 ?>
 <form action="save_edit_people.php" method="post">
-    <input type = "hidden" name = "id " value="<?php print($row["id"]);?>">
+    <input type = "hidden" name = "id" value="<?php print($row["id"]);?>">
     First Name:<input type="text" name="fname" value="<?php print($row["fname"]); ?>"><br>
     Last Name:<input type="text" name="lname" value="<?php print($row["lname"]); ?>"><br>
     Age:<input type="text" name="age" value="<?php print($row["age"]); ?>"><br>
@@ -18,7 +18,7 @@ if($result->num_rows > 0){
             <option value="F" <?php if($row["sex"]=="F") print("selected");?>>Female</option>
         </select><br>
     Marry Status:<select name="marry_status">
-            <option value="S" <?php if($row["marry_status"]=="S") print("selected");?>>Single</option>
+            <option value="N" <?php if($row["marry_status"]=="N") print("selected");?>>Single</option>
             <option value="M" <?php if($row["marry_status"]=="M") print("selected");?>>Married</option>
         </select><br>
     <input type="submit" value="edit">
--- list_people.php
+++ list_people.php
@@ -2,54 +2,72 @@
 
 
 require("connect_db.php");
-$sql = "SELECT id, fname, lname,age ,sex,marry_status FROM survey";
-$result = $conn->query($sql);
 
+if(isset($_GET["keyword"])){
+    $keyword=$_GET["keyword"];
+}else{
+    $keyword="";
+}
+
+if($keyword!=""){
+    $sql = "SELECT id, fname, lname, age, sex, marry_status FROM survey WHERE fname LIKE '%$keyword%' OR lname LIKE '%$keyword%'";
+}else{
+    $sql = "SELECT id, fname, lname, age, sex, marry_status FROM survey";
+}
+?>
+<form action="list_people.php" method="get">
+    Search:<input type="text" name="keyword" value="<?php print($keyword);?>">
+    <input type="submit" value="Search">
+</form><br>
+
+<?php
+$result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc())
    {
     if ( $row["age"]< 15 && $row["sex"]=="M"&&  $row["marry_status"]=="N")
     {
-        echo " เด็กชาย " . $row["fname"]. " " . $row["lname"].  "<a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+        echo " เด็กชาย " . $row["fname"]. " " . $row["lname"] ;
 
 
     }
     else if ($row["age"]>= 15 &&  $row["sex"]=="M"&&  $row["marry_status"]=="N")
     {
-        echo "นาย " . $row["fname"]. " " . $row["lname"]. "<a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+        echo "นาย " . $row["fname"]. " " . $row["lname"];
 
 
 
     }
     else if ($row["age"] >= 15 && $row["sex"]=="M"&&  $row["marry_status"]=="Y")
     {
-        echo  " นาย " . $row["fname"]. " " . $row["lname"].  "<a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+        echo  " นาย " . $row["fname"]. " " . $row["lname"];
 
 
     }
     else if($row["age"]< 15 &&$row["sex"]=="F"&&  $row["marry_status"]=="N")
     {
-        echo  " เด็กหญิง " . $row["fname"]. " " . $row["lname"]. "<a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+        echo  " เด็กหญิง " . $row["fname"]. " " . $row["lname"] ;
 
 
     }
     else if($row["age"] >= 15 &&$row["sex"]=="F"&&  $row["marry_status"]=="N")
     {
-        echo " นางสาว " . $row["fname"]. " " . $row["lname"]. "<a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+        echo " นางสาว " . $row["fname"]. " " . $row["lname"];
 
 
     }
     else if($row["age"] >= 15 && $row["sex"]=="F"&&  $row["marry_status"]=="Y")
     {
-        echo" นาง " . $row["fname"]. " " . $row["lname"]. "<a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+        echo" นาง " . $row["fname"]. " " . $row["lname"];
 
     }
    
-   
-  }
+    echo " <a href='edit_people.php?id=".$row["id"]."'>Edit</a> <a href='delete_people.php?id=".$row["id"]."' onclick=\"return confirm('Are you sure to delete ".$row["fname"]."')\".>Delete</a><br>";
+}
 } else {
   echo "0 results";
 }
 $conn->close();
 ?>
+<a href = "add_people.php">Add People</a>
